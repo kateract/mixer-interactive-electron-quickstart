4f59ea91188628e1707872be293e517f4228b341
Brian Shelledy <brian.shelledy@contalloy.com>
2017-05-15 11:58:22 -0500

    Fixed links opening in external windows

--- plugin/oauth-renderer.js
+++ plugin/oauth-renderer.js
@@ -1,8 +1,13 @@
 var ipcRenderer = require('electron').ipcRenderer;
 var remote = require('electron').remote;
+var shell = require('electron').shell;
 
 $('#saveButton').on('click', () => {
     var id=$('#clientID').val();
     var secret=$('#clientSecret').val();
     ipcRenderer.send('setOauthClientInfo', {id: id, secret: secret});
+})
+
+$('#beamoauth').on('click', () => {
+    shell.openExternal('https://beam.pro/lab/oauth');
 })
--- plugin/version-renderer.js
+++ plugin/version-renderer.js
@@ -1,7 +1,13 @@
 var ipcRenderer = require('electron').ipcRenderer;
 var remote = require('electron').remote;
+var shell = require('electron').shell;
+
 
 $('#saveButton').on('click', () => {
     var version=$('#versionID').val();
     ipcRenderer.send('setVersionInfo', version);
+})
+
+$('#beamstudio').on('click', () => {
+    shell.openExternal('https://beam.pro/i/studio');
 })
